1dbb05f3500192ef435274272729e52329ac3605
Frost <68116656+NooTheLee@users.noreply.github.com>
2022-07-13 16:56:34 +0700

    Update index.js

--- src/page/Layout/admin/index.js
+++ src/page/Layout/admin/index.js
@@ -1,26 +1,13 @@
-import {useAppContext} from "../../../context/useContext";
-import {Navigate} from "react-router-dom";
-const Admin = () => {
-    const {user} = useAppContext();
-
+import { useAppContext } from "../../../context/useContext";
+import { Navigate } from "react-router-dom";
+import { Admin } from "../../../components";
+const AdminPages = () => {
+    const { user } = useAppContext();
     if (user.role !== "Admin") {
-        return <Navigate to='/' />;
+        return <Navigate to="/" />;
     }
 
-    return (
-        <div className='w-screen h-screen pt-[65px] '>
-            <div className='mt-5 grid grid-cols-2 gap-x-5 mx-[10%] '>
-                <div className='col-span-1 h-[210px] w-full bg-[#90CAF9] rounded-[15px] text-center justify-center flex flex-col text-black '>
-                    <h1 className=' font-black text-[70px] '>100</h1>
-                    <h4 className='font-bold text-3xl '>people registered</h4>
-                </div>
-                <div className='col-span-1 h-[210px] w-full bg-[#80DEEA] rounded-[15px] text-center justify-center flex flex-col text-black '>
-                    <h1 className=' font-black text-[70px] '>200</h1>
-                    <h4 className='font-bold text-3xl '>post</h4>
-                </div>
-            </div>
-        </div>
-    );
+    return <Admin />;
 };
 
-export default Admin;
+export default AdminPages;
